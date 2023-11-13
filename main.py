e4aea04b2683c763091371bbc7a7310294acf50b
mergd <24722014+mergd@users.noreply.github.com>
2023-11-12 23:16:39 -0800

    fix:  port

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ def parseLlamalabels(data):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=int(os.getenv('PORT', 8000)))
+    uvicorn.run("main:app", host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
