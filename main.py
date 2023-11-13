b79503a0b859fa9ea6a718eae53c6808b691efd8
mergd <24722014+mergd@users.noreply.github.com>
2023-11-12 21:44:29 -0800

    fix for nixpacks

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ def parseLlamalabels(data):
 
 
 if __name__ == "__main__":
-    uvicorn.run("ethgpt:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
